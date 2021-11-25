af6de6071a9cdac663459f8c2f8be39fd7166450
Florian Mandlmeier <45791688+fm4991@users.noreply.github.com>
2021-11-25 16:46:04 +0100

    Update kubectl-start.sh

--- kubectl-start.sh
+++ kubectl-start.sh
@@ -1,3 +1,2 @@
 curl https://raw.githubusercontent.com/fm4991/kubectl-start/main/.kubectl-aliases -o ~/.kubectl-aliases
 grep -qxF "source ~/.kubectl-aliases" ~/.bashrc || echo "source ~/.kubectl-aliases" >> ~/.bashrc
-source ~/.bashrc
